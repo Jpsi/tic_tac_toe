15e0a0a1b049d4c6be657470f0df42018d433d49
Philip Fol <philip.fol08@gmail.com>
2020-01-02 23:18:48 +0000

    Replace repeated statements with array maps

--- src/components/Board.js
+++ src/components/Board.js
@@ -13,19 +13,13 @@ class Board extends React.Component {
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {[0, 1, 2].map((x) => this.renderSquare(x))}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {[3, 4, 5].map((x) => this.renderSquare(x))}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {[6, 7, 8].map((x) => this.renderSquare(x))}
         </div>
       </div>
     );
